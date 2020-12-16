a105ee4097f14b56753639467e234e1657d8957e
vika752 <71638053+vika752@users.noreply.github.com>
2020-12-16 16:57:51 +0300

    Update solar_vis.py

--- solar_vis.py
+++ solar_vis.py
@@ -9,10 +9,10 @@
 header_font = "Arial-16"
 """Шрифт в заголовке"""
 
-window_width = 800
+window_width = 600
 """Ширина окна"""
 
-window_height = 800
+window_height = 600
 """Высота окна"""
 
 scale_factor = None
@@ -33,9 +33,7 @@ def scale_x(x):
     Принимает вещественное число, возвращает целое число.
     В случае выхода **x** координаты за пределы экрана возвращает
     координату, лежащую за пределами холста.
-
     Параметры:
-
     **x** — x-координата модели.
     """
 
@@ -48,20 +46,15 @@ def scale_y(y):
     В случае выхода **y** координаты за пределы экрана возвращает
     координату, лежащую за пределами холста.
     Направление оси развёрнуто, чтобы у модели ось **y** смотрела вверх.
-
     Параметры:
-
     **y** — y-координата модели.
     """
-
-    return y  # FIXME: not done yet
+    return 600 - int(y*scale_factor) - window_height//2
 
 
 def create_star_image(space, star):
     """Создаёт отображаемый объект звезды.
-
     Параметры:
-
     **space** — холст для рисования.
     **star** — объект звезды.
     """
@@ -69,37 +62,34 @@ def create_star_image(space, star):
     x = scale_x(star.x)
     y = scale_y(star.y)
     r = star.R
-    star.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=star.color)
+    star.image = space.create_oval([x - r, y - r], [x + r, y + r], fill = star.color)
 
 
 def create_planet_image(space, planet):
     """Создаёт отображаемый объект планеты.
-
     Параметры:
-
     **space** — холст для рисования.
     **planet** — объект планеты.
     """
-    pass  # FIXME: сделать как у звезды
+    x = scale_x(planet.x)
+    y = scale_y(planet.y)
+    r = planet.R
+    planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill = planet.color)
 
 
 def update_system_name(space, system_name):
     """Создаёт на холсте текст с названием системы небесных тел.
     Если текст уже был, обновляет его содержание.
-
     Параметры:
-
     **space** — холст для рисования.
     **system_name** — название системы тел.
     """
-    space.create_text(30, 80, tag="header", text=system_name, font=header_font)
+    space.create_text(30, 80, tag="header", text = system_name, font = header_font)
 
 
 def update_object_position(space, body):
     """Перемещает отображаемый объект на холсте.
-
     Параметры:
-
     **space** — холст для рисования.
     **body** — тело, которое нужно переместить.
     """
